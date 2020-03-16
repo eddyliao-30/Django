5ef0023072fe15944fb11ac2470f655bceb12ba8
eddyliao-30 <eddyliao30@gmail.com>
2020-03-16 00:12:12 -0400

    work 3/16/2020

---
+++ mainsite/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+from datetime import datetime
+
+# Create your views here.
+def homepage(request):
+    posts = Post.objects.all()
+    now = datetime.now
+    post_lists = list()
+    for count,post in enumerate(posts):
+        post_lists.append("NO.{}:".format(str(count))+str(post)+"<br>")
+        post_lists.append("<small>"+str(post.body.encode('utf-8'))+"</small><br><br>")
+    return render(request,'index.html',locals())
--- mblog/settings.py
+++ mblog/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainsite',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +53,7 @@ ROOT_URLCONF = 'mblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
